3d98e637d0d256a28ad8fad67a769e11b4febd19
yash kumar <you@example.com>
2025-07-11 16:56:31 +0530

    use of document

---
+++ day22.js
@@ -0,0 +1,5 @@
+console.dir(document.getElementsByClassName('head'));
+console.dir(document.querySelector('h1'));
+console.dir(document.querySelector('#desc'));
+console.dir(document.querySelector('head'));
+console.dir(document.querySelector('ul li'));
